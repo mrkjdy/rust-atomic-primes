da759aa2d21e2aeab4c656a838939cccd9b0bad7
Mark Judy <mrkjudy@gmail.com>
2022-09-01 22:36:24 +0000

    feature: Added some benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,13 +1,84 @@
+use std::time::Duration;
+
 use criterion::{criterion_group, criterion_main, Criterion};
-use rust_atomic_primes::simple_soe;
+use rust_atomic_primes::{basic_threaded_soe, simple_soe};
 
-const MAX: usize = 1_000_000;
-
-fn simple_soe_benchmark(c: &mut Criterion) {
-    c.bench_function(&format!("simple_soe({})", MAX), |b| {
-        b.iter(|| crate::simple_soe(MAX))
+fn bench_1_000(c: &mut Criterion) {
+    let max = 1_000;
+    let mut group = c.benchmark_group(format!("max-{}", max));
+    group.sample_size(50);
+    group.bench_function(&format!("simple_soe({})", max), |b| {
+        b.iter(|| simple_soe(max))
     });
+    group.bench_function(&format!("basic_threaded_soe({}, 1)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 1))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 2)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 2))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 3)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 3))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 4)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 4))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 10)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 10))
+    });
+    group.finish();
 }
 
-criterion_group!(benches, simple_soe_benchmark);
+fn bench_1_000_000(c: &mut Criterion) {
+    let max = 1_000_000;
+    let mut group = c.benchmark_group(format!("max-{}", max));
+    group.sample_size(50);
+    group.bench_function(&format!("simple_soe({})", max), |b| {
+        b.iter(|| simple_soe(max))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 1)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 1))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 2)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 2))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 3)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 3))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 4)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 4))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 10)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 10))
+    });
+    group.finish();
+}
+
+fn bench_100_000_000(c: &mut Criterion) {
+    let max = 100_000_000;
+    let mut group = c.benchmark_group(format!("max-{}", max));
+    group
+        .sample_size(10)
+        .measurement_time(Duration::from_secs(12));
+    group.bench_function(&format!("simple_soe({})", max), |b| {
+        b.iter(|| simple_soe(max))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 1)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 1))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 2)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 2))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 3)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 3))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 4)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 4))
+    });
+    group.bench_function(&format!("basic_threaded_soe({}, 10)", max), |b| {
+        b.iter(|| basic_threaded_soe(max, 10))
+    });
+    group.finish();
+}
+
+criterion_group!(benches, bench_1_000, bench_1_000_000, bench_100_000_000);
 criterion_main!(benches);
