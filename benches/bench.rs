99161d95ab834720401568308317ac9877d80acb
Mark Judy <mrkjudy@gmail.com>
2022-09-03 18:00:41 -0400

    feature: Create bench for measuring performance of different sized vectors

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, time::Duration};
+use std::{fmt::Display, mem::size_of, time::Duration};
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, SamplingMode};
 use rust_atomic_primes::{basic_threaded_soe, simple_soe};
@@ -14,7 +14,7 @@ impl Display for MaxThreadStruct {
     }
 }
 
-fn bench(c: &mut Criterion) {
+fn sieve_bench(c: &mut Criterion) {
     let max_size_dur_tuples = [
         (1_000, 100, 5),
         (100_000, 100, 5),
@@ -43,5 +43,41 @@ fn bench(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, bench);
+fn cache_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("cache performance");
+    group.measurement_time(Duration::from_secs(10));
+    let exponents = 0..=20;
+    let mask_size_kb_pairs: Vec<(usize, usize)> = exponents
+        .map(|exponent| (!(usize::MAX << exponent + 3), 2_usize.pow(exponent)))
+        .collect();
+    for (mask, size_kb) in mask_size_kb_pairs {
+        let capacity = size_kb * (1024 / size_of::<usize>());
+        group.bench_with_input(
+            BenchmarkId::new("access_sim", size_kb),
+            &(capacity, mask),
+            |b, &(c, m)| {
+                let mut arr: Vec<usize> = Vec::with_capacity(c);
+                unsafe {
+                    arr.set_len(c);
+                }
+                b.iter(|| {
+                    const MAX: usize = 1_000_000;
+                    let mut sum = 0;
+                    let mut loc = 0;
+                    let mut del = 0;
+                    for _ in 0..MAX {
+                        let masked_loc = m & loc;
+                        arr[masked_loc] += 1;
+                        sum += arr[masked_loc];
+                        loc += del;
+                        del += sum;
+                    }
+                })
+            },
+        );
+    }
+    group.finish();
+}
+
+criterion_group!(benches, sieve_bench, cache_bench);
 criterion_main!(benches);
